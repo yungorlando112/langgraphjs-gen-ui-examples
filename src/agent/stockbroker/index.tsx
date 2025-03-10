1252961dd5c131cd15f7ec82f41e3cab19605ac6
bracesproul <braceasproul@gmail.com>
2025-03-10 11:01:31 -0700

    fix: Remove non agent code

---
+++ src/agent/stockbroker/index.tsx
@@ -0,0 +1,10 @@
+import { StateGraph, START } from "@langchain/langgraph";
+import { StockbrokerAnnotation } from "./types";
+import { callTools } from "./nodes/tools";
+
+const builder = new StateGraph(StockbrokerAnnotation)
+  .addNode("agent", callTools)
+  .addEdge(START, "agent");
+
+export const stockbrokerGraph = builder.compile();
+stockbrokerGraph.name = "Stockbroker";
--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,4 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
-import { StreamProvider } from "./providers/Stream.tsx";
-import { ThreadProvider } from "./providers/Thread.tsx";
-import { QueryParamProvider } from "use-query-params";
-import { ReactRouter6Adapter } from "use-query-params/adapters/react-router-6";
-import { BrowserRouter } from "react-router-dom";
-import { Toaster } from "@/components/ui/sonner";
 
-createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
-    <QueryParamProvider adapter={ReactRouter6Adapter}>
-      <ThreadProvider>
-        <StreamProvider>
-          <App />
-        </StreamProvider>
-      </ThreadProvider>
-    </QueryParamProvider>
-    <Toaster />
-  </BrowserRouter>,
-);
+createRoot(document.getElementById("root")!).render(<div>Hello world</div>);
